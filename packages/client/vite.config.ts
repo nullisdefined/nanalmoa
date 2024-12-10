45a8f53308c98bd37e0680be6a23df503a4811c0
“chae-dahee” <cdh010126r@gmail.com>
2024-12-11 02:12:32 +0900

    chore: vitest 세팅

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
@@ -17,4 +18,9 @@ export default defineConfig({
       },
     }),
   ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.ts',
+  },
 })
